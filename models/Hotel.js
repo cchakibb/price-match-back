0b91145b247e41a8ed3c083f060cb5e67e0026c1
cchakibb <chakibbachir88@gmail.com>
2020-07-31 18:33:04 +0200

    fix conflicts

--- models/Hotel.js
+++ models/Hotel.js
@@ -2,36 +2,20 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const hotelSchema = new Schema({
-<<<<<<< HEAD
-    
-        chk_in: String,
-        chk_out: String,
-        currency: String,
-        rates: [
-        {
-        code: String,
-        name: String,
-        rate: Number,
-        tax: Number,
-        },
-       
-        ],
-        hotel_url: [String],
-        }
-);
-
-const Hotel = mongoose.model("Hotel", hotelSchema);
-
-module.exports = Hotel;
-=======
   chk_in: String,
   chk_out: String,
   currency: String,
-  rates: [{ code: String, name: String, rate: Number, tax: Number }],
+  rates: [
+    {
+      code: String,
+      name: String,
+      rate: Number,
+      tax: Number,
+    },
+  ],
   hotel_url: [String],
 });
 
 const Hotel = mongoose.model("Hotel", hotelSchema);
 
 module.exports = Hotel;
->>>>>>> 26b19716149d25286762cd4c216ec9c4d5ee8a9e
