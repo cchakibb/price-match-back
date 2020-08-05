426ce1bdf6bf2c8c868978689a6d93308fa16707
cchakibb <chakibbachir88@gmail.com>
2020-08-05 18:44:41 +0200

    Your commit message

---
+++ public/precache-manifest.5b5e1a1d0b1c73121f56229ce90b4ce4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "295bc441de1ce3bf9c6b26a92dcfa8c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f242eb7acd4434f6a5b",
+    "url": "/static/css/2.8393d998.chunk.css"
+  },
+  {
+    "revision": "49d83967eaa937970532",
+    "url": "/static/css/main.07918569.chunk.css"
+  },
+  {
+    "revision": "2f242eb7acd4434f6a5b",
+    "url": "/static/js/2.168c813b.chunk.js"
+  },
+  {
+    "revision": "d22fc146c5d6401009fcb475495b87dc",
+    "url": "/static/js/2.168c813b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49d83967eaa937970532",
+    "url": "/static/js/main.7ffeb004.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
